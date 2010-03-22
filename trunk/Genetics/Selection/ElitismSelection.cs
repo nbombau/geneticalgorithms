4f36541af2d37f9c514a39a479bedc192a022e0b
elsilmaril <elsilmaril@e9cc63bc-2a90-461b-d780-69307b9a6340>
2010-03-22 23:26:23 +0000

    mejoras...el usuario puede selccionar porcentaje de mutaciones!

--- trunk/Genetics/Selection/ElitismSelection.cs
+++ trunk/Genetics/Selection/ElitismSelection.cs
@@ -5,10 +5,19 @@ using System.Text;
 
 namespace Genetics
 {
+    /// <summary>
+    /// Metodo de seleccion de elitismo
+    /// </summary>
     public class ElitismSelection : ISelection
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public ElitismSelection() { }
 
+        /// <summary>
+        /// Seleccionar un subconjunto de la poblacion, los <size> mejores
+        /// </summary>
         public void Select(IList<IIndividual> Individuals, int size)
         {
             IList<IIndividual> newIndividuals = new List<IIndividual>();
